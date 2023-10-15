be6b8be6f839b9e2124c7c56b0ae63611ff1c3ed
luche <luchen1201@hotmail.com>
2023-10-15 11:28:53 +0100

    Templates ad Service refactor

--- app/app.js
+++ app/app.js
@@ -1,67 +1,45 @@
-
 const express = require('express');
 const cors = require('cors');
-const userRouter = require("../router/bookRouter");
+const userRouter = require('../router/userRouter');
+const bookRouter = require('../router/bookRouter');
+
+const { connect } = require('../db/db');
+
+
 const app = express();
-//use middleware to form our contract for incoming json payloads ONLY!
+// use middleware to form our contract for incoming json payloads ONLY!!
 app.use(express.json());
-//use middleware for url encoding
-const {connect} = require('../db/db');
-app.use(express.urlencoded({extended:true}));
-//middleware to handle cors policy
-
+// use middleware for url encoding
+app.use(express.urlencoded({ extended: true }));
+// use middleware to handle cors policy
 app.use(cors());
-//HEALTH POINT OR accurator
-//https://localhost:3001
 
-/*** 
-app.use((req , res , next) => {
-    res.header("Access-Control-Allow-Origin ","*");
-    res.header("Access-Control-Allow-Origin-Headers","Content-Type , Accept , Authorication,Origin,X-Requiest-With");
-
-    if(req.method === "OPTIONS"){
-      res.header("Access-Control-Allow-Origin-Methods","POST,PUT,PATCH,DELETE,GET")
-
-    next();
-    };
-});
-***/
-
-app.get('/',(req,res,next) =>{
-
-    res.status(200).json({message : 'Service is up'})
-});
-//routers
-app.use('/users',userRouter);
-
-//build url or error we can handle
-//with middleware
-
-app.use((req , res,next) =>{
-const error = new Error("Not Found");
-error.status =404;
-next(error);
-
-
-
+// health point or actuator
+// http://localhost:3001
+app.get('/', (req, res, next) => {
+  res.status(200).json({ message: 'Service is up' });
 });
 
-    
+// routers
+app.use('/users', userRouter);
+app.use('/books', bookRouter);
 
 
-app.use((error,req,res,next) =>{
-   res.status(error.status || 500).json({
-       error:{
-           message: error.message,
-           status: error.status
 
-
-   },
-   });
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
 });
 
-
-
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: error.message,
+      status: error.status,
+    },
+  });
+});
 
 connect();
 
--- db/db.js
+++ db/db.js
@@ -1,31 +1,24 @@
-
+require('dotenv').config();
 const mongoose = require('mongoose');
 const User = require('../models/userModel');
-require('dotenv').config();
 
 const connect = async () => {
-    try {
-        await mongoose.connect(process.env.mongo);
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-    }
-}
-
-const disconnect = async() =>{
-
-    await mongooose.connection.close();
-};
-//obj: {req.body.email().....}
-const findUser = async(obj) =>{
-
-    User.findOnde(obj);
+  console.log('MongoDB is up and running');
+  mongoose.set('strictQuery', true);
+  await mongoose.connect(process.env.mongo);
 };
 
-const saveUser = async(newuser) =>{
+const disconnect = async () => {
+  await mongoose.connection.close();
+};
 
-    return await newuser.save();};
+// obj {firstName: req.body.firstName, email: req.body.email}
+const findUser = async (obj) => {
+  return User.findOne(obj).exec();
+};
 
+const saveUser = async (newUser) => {
+  return await newUser.save();
+};
 
-
-module.exports = {connect , disconnect , findUser , saveUser};
+module.exports = { connect, disconnect, findUser, saveUser };
